69d5d423c1eb286403894316aed7c2478e20044a
laura.leigh <lauracleigh314@gmail.com>
2022-10-04 14:53:40 +0100

    finishing present task

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,21 +1,23 @@
 require "present"
 
 RSpec.describe Present do
-    it  " wraps and unwraps a value" do 
+
+    it  "wraps and unwraps a value" do 
         present = Present.new
         present.wrap(6)
         expect(present.unwrap).to eq 6
     end
         
-        it "if we unwrap without wrapping first it fails" do
-            present = Present.new
+    it "if we unwrap without wrapping first it fails" do
+        present = Present.new
         expect {present.unwrap }.to raise_error "No contents have been wrapped."
-        end 
-        it "fails if we wrap if we already wrap" do
-            present = Present.new
-            present.wrap(6)
-        expect {present.wrap(7) }.to raise_error "No contents have been wrapped."
+    end 
+
+    it "fails if we wrap if we already wrap" do
+        present = Present.new
+        present.wrap(6)
+        expect {present.wrap(7) }.to raise_error "A contents has already been wrapped."
        
-        end 
+    end 
        
 end
